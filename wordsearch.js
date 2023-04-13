a85a927da2c7d6508d236356b8755a4cf203baf8
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-13 08:12:27 -0400

    wordseach assisted by @monki1

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  const newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    const newRow = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newRow.push(matrix[j][i]);
     }
-}
+    newMatrix.push(newRow);
+  }
+  return newMatrix;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
